b5aadf07290fe3f72236424bb170f8763a09ff14
Jia Hao Christian Chen <jiahao@itsincom.it>
2024-11-12 11:54:01 +0100

    category slug page fix

--- app/productsCategory/[slug]/page.js
+++ app/productsCategory/[slug]/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import { useRef, useState, useEffect } from 'react';
 import { FaArrowLeft, FaArrowRight, FaHandHoldingUsd } from "react-icons/fa";
 import { FaCalendarDays, FaClock } from "react-icons/fa6";
@@ -13,6 +13,8 @@ import Footer from '@/components/footer';
 import styles from './page.module.css';
 
 export default function Products() {
+
+  const { slug } = useParams();
 
   const containerRef = useRef(null);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -48,45 +50,45 @@ export default function Products() {
     // Funzione per ottenere i prodotti dal backend
     const fetchProducts = async () => {
       try {
-        const response = await fetch(`http://localhost:8080/products?categoria=${slug}`, {
-          method: "GET",
-          credentials: 'include',
-        });
+        const response = await fetch(`http://localhost:8080/products/${slug}`, {
+        method: "GET",
+            credentials: 'include',
+      });
 
-        if (!response.ok) {
-          throw new Error("Errore durante il recupero dei prodotti. Riprova più tardi.");
-        }
-
-        const data = await response.json();
-        setProducts(data);
-        setLoading(false);
-      } catch (error) {
-        console.error(error.message);
-      } finally {
-        setLoading(false);
+      if (!response.ok) {
+        throw new Error("Errore durante il recupero dei prodotti. Riprova più tardi.");
       }
-    };
 
-    fetchProducts();
-  }, []);
-
-  // Funzione per gestire il click del bottone "Ordina ora"
-  const handleOrderClick = (productName) => {
-    const slug = generateSlug(productName); // Genera lo slug
-    router.push(`/prodotti/${slug}`); // Naviga alla pagina dinamica
+      const data = await response.json();
+      setProducts(data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  // Funzione per generare lo slug
-  const generateSlug = (name) => {
-    return name
+  fetchProducts();
+}, [slug]);
+
+// Funzione per gestire il click del bottone "Ordina ora"
+const handleOrderClick = (productName) => {
+  const slug = generateSlug(productName); // Genera lo slug
+  router.push(`/prodotti/${slug}`);
+};
+
+// Funzione per generare lo slug
+const generateSlug = (name) => {
+  return name
       .toLowerCase()
       .replace(/\s+/g, '-') // Sostituisce gli spazi con trattini
       .replace(/[^\w\-]+/g, '') // Rimuove caratteri speciali
       .replace(/\-\-+/g, '-') // Rimuove trattini consecutivi
       .trim(); // Rimuove spazi extra
-  };
+};
 
-  return (
+return (
     <div>
       <header>
         <Header />
@@ -100,9 +102,9 @@ export default function Products() {
         </div>
         <section className={styles.featureSection} ref={containerRef}>
           <button
-            className={styles.arrowButtonLeft}
-            onClick={() => handleScroll('left')}
-            disabled={currentIndex === 0}>
+              className={styles.arrowButtonLeft}
+              onClick={() => handleScroll('left')}
+              disabled={currentIndex === 0}>
             <FaArrowLeft />
           </button>
           <div className={styles.featuresContainer}>
@@ -126,9 +128,9 @@ export default function Products() {
             </div>
           </div>
           <button
-            className={styles.arrowButtonRight}
-            onClick={() => handleScroll('right')}
-            disabled={currentIndex === steps.length - 1}>
+              className={styles.arrowButtonRight}
+              onClick={() => handleScroll('right')}
+              disabled={currentIndex === steps.length - 1}>
             <FaArrowRight />
           </button>
         </section>
@@ -137,34 +139,34 @@ export default function Products() {
           <h2>I nostri prodotti</h2>
           <div className={styles.items}>
             {error ? (
-              <div>
-                <p>Errore durante il caricamento dei prodotti. Riprova più tardi.</p>
-                <button onClick={fetchProducts}>Riprova</button>
-              </div>
+                <div>
+                  <p>Errore durante il caricamento dei prodotti. Riprova più tardi.</p>
+                  <button onClick={fetchProducts}>Riprova</button>
+                </div>
             ) : loading ? (
-              <div>Caricamento in corso...</div>
+                <div>Caricamento in corso...</div>
             ) : (
-              <div>
-                {products.length === 0 ? (
-                  <p>Prodotti terminati. Torna presto per nuovi dolci!</p>
-                ) : (
-                  products.map((product, index) => (
-                    <div key={index} className={styles.item} onClick={() => handleOrderClick(product.name)}>
-                      <img src={product.image} alt={product.name} width='400' height='300' />
-                      <div className={styles.itemDetails}>
-                        <div className={styles.textDetails}>
-                          <p>{product.name}</p>
-                          <span>{product.price}€</span>
-                        </div>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='40' height='40' fill="#000">
-                          <path d="M5 8h16l-2 7H7L5 8z" fill="#F3BC9F"></path>
-                          <path d="M9 18c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2ZM17 18c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2ZM21 7H5.75l-.79-2.77A1 1 0 0 0 4 3.5H3c-.55 0-1 .45-1 1s.45 1 1 1h.25l2.48 8.69A2.5 2.5 0 0 0 8.13 16h9.74a2.5 2.5 0 0 0 2.4-1.81l1.69-5.91A1 1 0 0 0 21 7Zm-2.65 6.64a.5.5 0 0 1-.48.36H8.13a.5.5 0 0 1-.48-.36L6.33 9h13.35l-1.33 4.64Z"></path>
-                        </svg>
-                      </div>
-                    </div>
-                  ))
-                )}
-              </div>
+                <div>
+                  {products.length === 0 ? (
+                      <p>Prodotti terminati. Torna presto per nuovi dolci!</p>
+                  ) : (
+                      products.map((product, index) => (
+                          <div key={index} className={styles.item} onClick={() => handleOrderClick(product.name)}>
+                            <img src={product.image} alt={product.name} width='400' height='300' />
+                            <div className={styles.itemDetails}>
+                              <div className={styles.textDetails}>
+                                <p>{product.name}</p>
+                                <span>{product.price}€</span>
+                              </div>
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width='40' height='40' fill="#000">
+                                <path d="M5 8h16l-2 7H7L5 8z" fill="#F3BC9F"></path>
+                                <path d="M9 18c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2ZM17 18c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2ZM21 7H5.75l-.79-2.77A1 1 0 0 0 4 3.5H3c-.55 0-1 .45-1 1s.45 1 1 1h.25l2.48 8.69A2.5 2.5 0 0 0 8.13 16h9.74a2.5 2.5 0 0 0 2.4-1.81l1.69-5.91A1 1 0 0 0 21 7Zm-2.65 6.64a.5.5 0 0 1-.48.36H8.13a.5.5 0 0 1-.48-.36L6.33 9h13.35l-1.33 4.64Z"></path>
+                              </svg>
+                            </div>
+                          </div>
+                      ))
+                  )}
+                </div>
             )}
           </div>
         </section>
@@ -174,5 +176,5 @@ export default function Products() {
         <Footer />
       </footer>
     </div >
-  );
+);
 }
