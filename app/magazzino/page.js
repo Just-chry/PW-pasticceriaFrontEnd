b8636afff28941cf466975dc97573242aaaf0896
“Leonardo <sl.falese@gmail.com>
2024-11-14 03:06:25 +0100

    fix visible magazzino, fix button delete ordiniUtente, add ordiniGiornalieri

--- app/magazzino/page.js
+++ app/magazzino/page.js
@@ -1,18 +1,18 @@
 "use client";
 
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Hero from '@/components/hero';
 import Footer from '@/components/footer';
 import styles from './page.module.css';
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Prodotti() {
     const router = useRouter();
     const [userRole, setUserRole] = useState(null);
     const [products, setProducts] = useState([]);
     const [isOpen, setIsOpen] = useState(false);
-    const [editProductModal, setEditProductModal] = useState({isOpen: false, product: null});
+    const [editProductModal, setEditProductModal] = useState({ isOpen: false, product: null });
     const [newProduct, setNewProduct] = useState({
         name: '',
         image: '',
@@ -89,7 +89,7 @@ export default function Prodotti() {
         if (file) {
             const reader = new FileReader();
             reader.onloadend = () => {
-                setNewProduct((prevState) => ({...prevState, image: file, previewImage: reader.result}));
+                setNewProduct((prevState) => ({ ...prevState, image: file, previewImage: reader.result }));
             };
             reader.readAsDataURL(file);
         }
@@ -152,11 +152,11 @@ export default function Prodotti() {
     };
 
     const handleModifyProduct = (product) => {
-        setEditProductModal({isOpen: true, product});
+        setEditProductModal({ isOpen: true, product });
     };
 
     const handleEditProductChange = (e) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         setEditProductModal(prevState => ({
             ...prevState,
             product: {
@@ -168,7 +168,7 @@ export default function Prodotti() {
 
     const handleEditProductSave = async () => {
         try {
-            const {product} = editProductModal;
+            const { product } = editProductModal;
 
             const updatedProductData = {
                 ...product,
@@ -191,7 +191,7 @@ export default function Prodotti() {
 
             await fetchProducts();
 
-            setEditProductModal({isOpen: false, product: null});
+            setEditProductModal({ isOpen: false, product: null });
         } catch (error) {
             console.error('Errore:', error.message);
             alert(`Errore durante la modifica del prodotto: ${error.message}`);
@@ -247,7 +247,7 @@ export default function Prodotti() {
             setProducts((prevProducts) =>
                 prevProducts.map(product =>
                     product.id === id
-                        ? {...product, quantity: product.quantity + increment}
+                        ? { ...product, quantity: product.quantity + increment }
                         : product
                 )
             );
@@ -265,7 +265,7 @@ export default function Prodotti() {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({is_visible: isVisible}), // Cambiato per inviare `is_visible`
+                body: JSON.stringify({ is_visible: isVisible }), // Cambiato per inviare `is_visible`
             });
 
             if (!response.ok) {
@@ -274,7 +274,7 @@ export default function Prodotti() {
 
             setProducts((prevProducts) =>
                 prevProducts.map(product =>
-                    product.id === id ? {...product, isVisible: isVisible} : product
+                    product.id === id ? { ...product, isVisible: isVisible } : product
                 )
             );
 
@@ -287,7 +287,7 @@ export default function Prodotti() {
 
     return (
         <div>
-            <Hero/>
+            <Hero />
             <main className={styles.main}>
                 <h1 className={styles.title}>Il nostro magazzino</h1>
 
@@ -346,7 +346,7 @@ export default function Prodotti() {
                                 type="text"
                                 placeholder="Nome del Prodotto"
                                 value={newProduct.name}
-                                onChange={(e) => setNewProduct({...newProduct, name: e.target.value})}
+                                onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
                                 className={styles.input}
                             />
                             <input
@@ -359,27 +359,27 @@ export default function Prodotti() {
                                 type="text"
                                 placeholder="Prezzo"
                                 value={newProduct.price}
-                                onChange={(e) => setNewProduct({...newProduct, price: e.target.value})}
+                                onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
                                 className={styles.input}
                             />
                             <input
                                 type="text"
                                 placeholder="Ingredienti"
                                 value={newProduct.ingredients}
-                                onChange={(e) => setNewProduct({...newProduct, ingredients: e.target.value})}
+                                onChange={(e) => setNewProduct({ ...newProduct, ingredients: e.target.value })}
                                 className={styles.input}
                             />
                             <input
                                 type="text"
                                 placeholder="Descrizione"
                                 value={newProduct.description}
-                                onChange={(e) => setNewProduct({...newProduct, description: e.target.value})}
+                                onChange={(e) => setNewProduct({ ...newProduct, description: e.target.value })}
                                 className={styles.input}
                             />
 
                             <select
                                 value={newProduct.category}
-                                onChange={(e) => setNewProduct({...newProduct, category: e.target.value})}
+                                onChange={(e) => setNewProduct({ ...newProduct, category: e.target.value })}
                                 className={styles.input}
                             >
                                 <option value="">Seleziona una categoria</option>
@@ -404,7 +404,7 @@ export default function Prodotti() {
                                 type="number"
                                 placeholder="Quantità"
                                 value={newProduct.quantity}
-                                onChange={(e) => setNewProduct({...newProduct, quantity: parseInt(e.target.value, 10)})}
+                                onChange={(e) => setNewProduct({ ...newProduct, quantity: parseInt(e.target.value, 10) })}
                                 className={styles.input}
                             />
 
@@ -431,7 +431,7 @@ export default function Prodotti() {
                     <div className={styles.modal}>
                         <div className={styles.modalContent}>
                             <span className={styles.close}
-                                  onClick={() => setEditProductModal({isOpen: false, product: null})}>
+                                onClick={() => setEditProductModal({ isOpen: false, product: null })}>
                                 &times;
                             </span>
                             <h2>Modifica Prodotto</h2>
@@ -515,13 +515,13 @@ export default function Prodotti() {
                 )}
             </main>
             <footer>
-                <Footer/>
+                <Footer />
             </footer>
         </div>
     );
 }
 
-function ProductCard({product, onModify, onDelete, onQuantityChange, onVisibilityChange}) {
+function ProductCard({ product, onModify, onDelete, onQuantityChange, onVisibilityChange }) {
     if (!product) {
         return null;
     }
@@ -549,64 +549,65 @@ function ProductCard({product, onModify, onDelete, onQuantityChange, onVisibilit
                 </p>
                 <p className={styles.productDescription}><span>Descrizione:</span> {product.description}</p>
                 <p className={styles.productCategory}><span>Categoria:</span> {product.category}</p>
+                <div className={styles.visibleAndQuantity}>
+                    <div className={styles.checkboxContainer}>
+                        <input
+                            type="checkbox"
+                            checked={Boolean(product.isVisible)}
+                            onChange={() => onVisibilityChange(product.id, !product.isVisible)}
+                            className={styles.checkbox}
+                        />
+                        <label>Visibile agli utenti</label>
+                    </div>
 
-                <div className={styles.checkboxContainer}>
-                    <input
-                        type="checkbox"
-                        checked={Boolean(product.isVisible)}
-                        onChange={() => onVisibilityChange(product.id, !product.isVisible)}
-                        className={styles.checkbox}
-                    />
-                    <label>Visibile agli utenti</label>
+                    <div className={styles.quantityControls}>
+                        <button onClick={() => onQuantityChange(product.id, -1)} className={styles.quantityButton}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                width="20"
+                                height="20"
+                                fill="#F3BC9F">
+                                <path d="M20 11H4c-.55 0-1 .45-1 1s.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1Z"></path>
+                            </svg>
+                        </button>
+                        <span className={styles.quantityDisplay}>{product.quantity}</span>
+                        <button onClick={() => onQuantityChange(product.id, 1)} className={styles.quantityButton}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 480 480"
+                                width="20"
+                                height="20"
+                                fill="#F3BC9F">
+                                <path
+                                    d="M450 210H270V30a30 30 0 1 0-60 0v180H30a30 30 0 1 0 0 60h180v180a30 30 0 1 0 60 0V270h180a30 30 0 1 0 0-60Z"></path>
+                            </svg>
+                        </button>
+                    </div>
                 </div>
+                <button className={styles.deleteButton} onClick={() => onDelete(product.id)}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="24"
+                        height="24"
+                        fill="#000">
+                        <path d="m5 7 1 14h12l1-14H5z" fill="#F3BC9F"></path>
+                        <path
+                            d="M20 6h-4V4.5A2.5 2.5 0 0 0 13.5 2h-3A2.5 2.5 0 0 0 8 4.5V6H4c-.55 0-1 .45-1 1s.45 1 1 1h.12l.73 11.66A2.49 2.49 0 0 0 7.35 22h9.3c1.32 0 2.41-1.03 2.5-2.34L19.88 8H20c.55 0 1-.45 1-1s-.45-1-1-1ZM10 4.5c0-.28.22-.5.5-.5h3c.28 0 .5.22.5.5V6h-4V4.5Zm7.15 15.03a.51.51 0 0 1-.5.47h-9.3a.51.51 0 0 1-.5-.47L6.13 8h11.74l-.72 11.53ZM10 10c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1s1-.45 1-1v-6c0-.55-.45-1-1-1ZM14 10c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1s1-.45 1-1v-6c0-.55-.45-1-1-1Z"></path>
+                    </svg>
+                </button>
 
-                <div className={styles.quantityControls}>
-                    <button onClick={() => onQuantityChange(product.id, -1)} className={styles.quantityButton}>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            width="24"
-                            height="24"
-                            fill="#F3BC9F">
-                            <path d="M20 11H4c-.55 0-1 .45-1 1s.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1Z"></path>
-                        </svg>
-                    </button>
-                    <span className={styles.quantityDisplay}>{product.quantity}</span>
-                    <button onClick={() => onQuantityChange(product.id, 1)} className={styles.quantityButton}>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 480 480"
-                            width="24"
-                            height="24"
-                            fill="#F3BC9F">
-                            <path
-                                d="M450 210H270V30a30 30 0 1 0-60 0v180H30a30 30 0 1 0 0 60h180v180a30 30 0 1 0 60 0V270h180a30 30 0 1 0 0-60Z"></path>
-                        </svg>
-                    </button>
-                </div>
+                <button className={styles.modifyButton} onClick={() => onModify(product)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#000">
+                        <path
+                            d="m21 9-2-3-4 1L14 3h-4L9 6.4 5 6 3 9l3 3-3 3 2 3h4l1 3 4 0 1.5-3.61L19 18l1.5-3-2.4-3L21 9Zm-7 5h-4v-4h4v4Z"
+                            fill="#F3BC9F"></path>
+                        <path
+                            d="M12 22c-.61 0-1.23-.06-1.86-.18a1.5 1.5 0 0 1-1.18-1.75 1.48 1.48 0 0 0-2.43-1.41c-.61.53-1.56.49-2.1-.15a10.03 10.03 0 0 1-1.86-3.21 1.5 1.5 0 0 1 .93-1.9c.6-.21 1-.77 1-1.41s-.4-1.2-1-1.41a1.48 1.48 0 0 1-.92-1.9 10 10 0 0 1 1.85-3.21 1.5 1.5 0 0 1 2.11-.14 1.49 1.49 0 0 0 2.43-1.41 1.5 1.5 0 0 1 1.17-1.75 9.7 9.7 0 0 1 3.72 0c.8.15 1.33.94 1.18 1.75a1.48 1.48 0 0 0 2.43 1.4 1.48 1.48 0 0 1 2.1.14 9.97 9.97 0 0 1 1.86 3.22 1.5 1.5 0 0 1-.93 1.9c-.6.21-1 .77-1 1.41s.41 1.2 1 1.41c.77.26 1.2 1.11.92 1.9a10 10 0 0 1-1.85 3.21 1.5 1.5 0 0 1-2.11.14 1.51 1.51 0 0 0-1.72-.17c-.55.32-.83.95-.71 1.57a1.5 1.5 0 0 1-1.18 1.75c-.63.12-1.25.18-1.86.18Zm-1-2.07c.68.09 1.34.09 2.02 0a3.5 3.5 0 0 1 5.35-3.09c.41-.54.75-1.12 1-1.75a3.49 3.49 0 0 1 0-6.18 7.81 7.81 0 0 0-1-1.75 3.5 3.5 0 0 1-5.35-3.09 7.59 7.59 0 0 0-2.02 0 3.5 3.5 0 0 1-5.35 3.09 8.04 8.04 0 0 0-1 1.75 3.49 3.49 0 0 1 0 6.18c.26.62.6 1.21 1 1.75a3.5 3.5 0 0 1 5.35 3.09ZM12 15c-1.65 0-3-1.35-3-3s1.35-3 3-3 3 1.35 3 3-1.35 3-3 3Zm0-4c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1Z"></path>
+                    </svg>
+                </button>
             </div>
-            <button className={styles.deleteButton} onClick={() => onDelete(product.id)}>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24"
-                    height="24"
-                    fill="#000">
-                    <path d="m5 7 1 14h12l1-14H5z" fill="#F3BC9F"></path>
-                    <path
-                        d="M20 6h-4V4.5A2.5 2.5 0 0 0 13.5 2h-3A2.5 2.5 0 0 0 8 4.5V6H4c-.55 0-1 .45-1 1s.45 1 1 1h.12l.73 11.66A2.49 2.49 0 0 0 7.35 22h9.3c1.32 0 2.41-1.03 2.5-2.34L19.88 8H20c.55 0 1-.45 1-1s-.45-1-1-1ZM10 4.5c0-.28.22-.5.5-.5h3c.28 0 .5.22.5.5V6h-4V4.5Zm7.15 15.03a.51.51 0 0 1-.5.47h-9.3a.51.51 0 0 1-.5-.47L6.13 8h11.74l-.72 11.53ZM10 10c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1s1-.45 1-1v-6c0-.55-.45-1-1-1ZM14 10c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1s1-.45 1-1v-6c0-.55-.45-1-1-1Z"></path>
-                </svg>
-            </button>
-
-            <button className={styles.modifyButton} onClick={() => onModify(product)}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#000">
-                    <path
-                        d="m21 9-2-3-4 1L14 3h-4L9 6.4 5 6 3 9l3 3-3 3 2 3h4l1 3 4 0 1.5-3.61L19 18l1.5-3-2.4-3L21 9Zm-7 5h-4v-4h4v4Z"
-                        fill="#F3BC9F"></path>
-                    <path
-                        d="M12 22c-.61 0-1.23-.06-1.86-.18a1.5 1.5 0 0 1-1.18-1.75 1.48 1.48 0 0 0-2.43-1.41c-.61.53-1.56.49-2.1-.15a10.03 10.03 0 0 1-1.86-3.21 1.5 1.5 0 0 1 .93-1.9c.6-.21 1-.77 1-1.41s-.4-1.2-1-1.41a1.48 1.48 0 0 1-.92-1.9 10 10 0 0 1 1.85-3.21 1.5 1.5 0 0 1 2.11-.14 1.49 1.49 0 0 0 2.43-1.41 1.5 1.5 0 0 1 1.17-1.75 9.7 9.7 0 0 1 3.72 0c.8.15 1.33.94 1.18 1.75a1.48 1.48 0 0 0 2.43 1.4 1.48 1.48 0 0 1 2.1.14 9.97 9.97 0 0 1 1.86 3.22 1.5 1.5 0 0 1-.93 1.9c-.6.21-1 .77-1 1.41s.41 1.2 1 1.41c.77.26 1.2 1.11.92 1.9a10 10 0 0 1-1.85 3.21 1.5 1.5 0 0 1-2.11.14 1.51 1.51 0 0 0-1.72-.17c-.55.32-.83.95-.71 1.57a1.5 1.5 0 0 1-1.18 1.75c-.63.12-1.25.18-1.86.18Zm-1-2.07c.68.09 1.34.09 2.02 0a3.5 3.5 0 0 1 5.35-3.09c.41-.54.75-1.12 1-1.75a3.49 3.49 0 0 1 0-6.18 7.81 7.81 0 0 0-1-1.75 3.5 3.5 0 0 1-5.35-3.09 7.59 7.59 0 0 0-2.02 0 3.5 3.5 0 0 1-5.35 3.09 8.04 8.04 0 0 0-1 1.75 3.49 3.49 0 0 1 0 6.18c.26.62.6 1.21 1 1.75a3.5 3.5 0 0 1 5.35 3.09ZM12 15c-1.65 0-3-1.35-3-3s1.35-3 3-3 3 1.35 3 3-1.35 3-3 3Zm0-4c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1Z"></path>
-                </svg>
-            </button>
         </div>
     );
 }
